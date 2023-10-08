c90a802d1ea33b8ccae910155fe6b90e767fc917
andresqb198 <andresqb198@gmail.com>
2023-10-07 20:15:34 -0500

    Add processing baseline

--- Holistic.py
+++ Holistic.py
@@ -4,62 +4,64 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
+def transform_video(video, name:str):
+    with mp_holistic.Holistic(
+        static_image_mode=False,
+        model_complexity=2) as holistic:
+        _process_video(video, holistic)
 
-video = cv2.VideoCapture('./Raw_Data/1/S1V1C1M1A Clip1.mp4')
-
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
-
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Puedes cambiar el codec según tus necesidades
-out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
-
-
-with mp_holistic.Holistic(
-    static_image_mode=False,
-    model_complexity=2
-) as holistic:
-    
+def _process_video(video, holistic):
     while True:
         ret, frame = video.read()
         if ret == False:
             break
-
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = holistic.process(frame_rgb)
-
-        #Rostro
-        mp_drawing.draw_landmarks(frame, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
-                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
-                             ) 
-        # Draw pose connections
-        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                ) 
-        # Draw left hand connections
-        mp_drawing.draw_landmarks(frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-                                mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                ) 
-        # Draw right hand connections  
-        mp_drawing.draw_landmarks(frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                )
-
+        _draw_landmarks(frame, results,"RHand")
+        _draw_landmarks(frame, results,"LHand")
+        _draw_landmarks(frame, results,"Pose")
+        _draw_landmarks(frame, results,"Face")
     
-
-
-
-        cv2.imshow('Frame', frame)
-        out.write(frame)
-
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
     video.release()
-    out.release() 
     cv2.destroyAllWindows()
+
+
+def _return_configurations(type:str,results):
+    configuration_draw = {
+        "RHand":{
+            "results":results.right_hand_landmarks,
+            "connections": mp_holistic.HAND_CONNECTIONS,
+            "drawing1": mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+            "drawing2":mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+        },
+        "LHand":{
+            "results":results.left_hand_landmarks,
+            "connections": mp_holistic.HAND_CONNECTIONS,
+            "drawing1":  mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+            "drawing2":mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+        },
+        "Pose":{
+            "results":results.left_hand_landmarks,
+            "connections": mp_holistic.POSE_CONNECTIONS,
+            "drawing1": mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+            "drawing2":mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+        },
+        "Face":{
+            "results":results.face_landmarks,
+            "connections": mp_holistic.FACEMESH_TESSELATION,
+            "drawing1": mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+            "drawing2":mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+        }
+    }
+    
+    return configuration_draw[type]
+
+def _draw_landmarks(frame, results, type:str):
+    configuration = _return_configurations(type,results)
+    mp_drawing.draw_landmarks(frame, configuration["results"], configuration["connections"], 
+                                    configuration["drawing1"], 
+                                    configuration["drawing2"]
+                                    )
+
 
     
